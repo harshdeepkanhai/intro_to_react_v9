95e9748a52081405db0af2e2c02a9868ec16bb1a
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-10-31 05:10:20 +0530

    Make Reusable Component Pizza

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,11 +1,21 @@
-const App = () => {
-    return React.createElement(
-        "div",
-        {},
-        React.createElement("h1", {}, "Padre Gino's")
-    )
+const Pizza = () => {
+    return React.createElement("div", {} , [
+        React.createElement("h1", {}, "The Pepperoni Pizza"),
+        React.createElement("p", {}, "Mozzarella Cheese, Pepperoni"),
+    ])
 }
 
-const container = document.getElementById("root")
+const App = () => {
+    return React.createElement("div", {},[
+        React.createElement("h1", {}, "Padre Gino's"),
+        React.createElement(Pizza),
+        React.createElement(Pizza),
+        React.createElement(Pizza),
+        React.createElement(Pizza),
+        React.createElement(Pizza),
+    ]);
+};
+
+const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
 root.render(React.createElement(App));
