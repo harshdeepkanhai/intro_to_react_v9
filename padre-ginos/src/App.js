8664ebac3b98d6a59a3abaf87a23a232284fe70e
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-10-31 05:15:38 +0530

    Add props to the components

--- padre-ginos/src/App.js
+++ padre-ginos/src/App.js
@@ -1,18 +1,33 @@
-const Pizza = () => {
+const Pizza = (props) => {
     return React.createElement("div", {} , [
-        React.createElement("h1", {}, "The Pepperoni Pizza"),
-        React.createElement("p", {}, "Mozzarella Cheese, Pepperoni"),
+        React.createElement("h1", {}, props.name),
+        React.createElement("p", {}, props.description),
     ])
 }
 
 const App = () => {
     return React.createElement("div", {},[
         React.createElement("h1", {}, "Padre Gino's"),
-        React.createElement(Pizza),
-        React.createElement(Pizza),
-        React.createElement(Pizza),
-        React.createElement(Pizza),
-        React.createElement(Pizza),
+        React.createElement(Pizza, {
+            name: "The Pepperoni Pizza",
+            description: "some dope pizza yo",
+        }),
+        React.createElement(Pizza, {
+            name: "Americano Pizza",
+            description: "French fries and hot dogs, wtf Italy",
+        }),
+        React.createElement(Pizza, {
+            name: "The Hawaiian",
+            description: "pineapple and ham, wtf America",
+        }),
+        React.createElement(Pizza, {
+            name: "Chicken Pizza",
+            description: "chicken nuggies on your pizza, wtf UK",
+        }),
+        React.createElement(Pizza, {
+            name: "Baked Potato Pizza",
+            description: "unholy potato mash, wtf Minnesota",
+        }),
     ]);
 };
 
