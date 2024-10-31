6b38e92c04b7378978f337aed464325371ec7c39
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-10-31 09:16:48 +0530

    Add useContext and createContext

--- padre-ginos/src/usePizzaOfTheDay.jsx
+++ padre-ginos/src/usePizzaOfTheDay.jsx
@@ -1,8 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useDebugValue } from "react";
 
 export const usePizzaOfTheDay = () => {
   const [pizzaOfTheDay, setPizzaOfTheDay] = useState(null);
-
+  //   useDebugValue(pizzaOfTheDay);
   useEffect(() => {
     async function fetchPizzaOfTheDay() {
       //   await new Promise((resolve) => setTimeout(resolve, 10000));
