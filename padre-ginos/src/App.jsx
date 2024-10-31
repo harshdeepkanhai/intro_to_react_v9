c7686188d5e69ebc0ce7455b994d0b460583b1c9
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2024-10-31 07:08:51 +0530

    Create components without state

--- padre-ginos/src/App.jsx
+++ padre-ginos/src/App.jsx
@@ -1,13 +1,11 @@
 import { createRoot } from "react-dom";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 const App = () => {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza name="Pepperoni" description="pep, cheese, n stuff" />
-      <Pizza name="Hawaiian" description="ham, pineapple, n stuff" />
-      <Pizza name="Americano" description="french fries, hot dogs, n stuff" />
+      <Order />
     </div>
   );
 };
